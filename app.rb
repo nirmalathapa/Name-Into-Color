65114a38c84f8b233c7ced300a2985f6b2326598
Rajesh Sharma <rajesh@surf.net.np>
2016-10-08 23:56:07 +0200

    Add endpoint to handle name to color conversion

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'sinatra/json'
+require_relative 'color'
 
 get '/' do
-    File.read(File.join('public', 'index.html'))
+  File.read(File.join('public', 'index.html'))
+end
+
+post '/api/colors' do
+  color = Color.new params['name']
+  json color.hex_colors
 end
---
+++ color.rb
@@ -0,0 +1,13 @@
+class Color
+  attr_reader :name, :numbers
+
+  def initialize name
+    @numbers = ''
+    @name = name
+  end
+
+  def hex_colors
+    name.each_char { |c| numbers << c.ord.to_s(16)  }
+    numbers.split(//).each_slice(6).map { |g| g.join.rjust(6, '0') }
+  end
+end

49aafe64f406d4623759b521074e585a70621fe7
nirmala <nirmala@surf.net.np>
2016-10-12 22:20:45 +0200

    fix content type

--- app.rb
+++ app.rb
@@ -2,17 +2,13 @@ require 'sinatra'
 require 'sinatra/json'
 require_relative 'color'
 
-before do
-   content_type :json
-   headers 'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']  
-end
-
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
 post '/api/colors' do
+   headers 'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']  
   color = Color.new params['name']
   json color.hex_colors
 end
